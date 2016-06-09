1ecb6cf88d5f03a763c4fb0cf579cf254302176f
Nicola Sanitate <n.sanitate@gmail.com>
2016-06-09 22:38:03 +0200

    All features completed

--- source/calculator.js
+++ source/calculator.js
@@ -1,40 +1,74 @@
 class Calculator {
 
-  add(numbers) {
+    add(inputString) {
+        if (this.isNotString(inputString))
+            this.invalidArgumentError();
 
-    if (typeof (numbers) !== 'string')
-      this.invalidArgumentException();
+        if (inputString.length === 0)
+            return 0;
 
-    if (numbers.length === 0) 
-      return 0;
+        let {numbersString, separator} = this.splitSeparator(inputString);
 
-    let separator = new RegExp(',|\n');
+        if (this.isNotWellFormed(numbersString, separator))
+            this.invalidArgumentError();
 
-    if (numbers.indexOf('//') === 0) {
-      
-      let parts = numbers.split('\n')[0];
-        separator = parts.replace(/^\/\//, '').replace('\n', '');
+        let numbers = this.extractNumbers(numbersString, separator);
 
-        numbers = parts.slice(1);
+        if (this.containsNegativeNumber(numbers)) {
+            this.negativesNotAllowedError();
+        }
 
-        console.log(numbers);
+        return this.getTotal(numbers, separator);
     }
-    
-    let total = numbers.split(separator)
-      .map((item) => parseInt(item))
-      .reduce((memo, item) => memo + item);
 
-    if (isNaN(total))
-      this.invalidArgumentException();
+    isNotString(inputString) {
+        return typeof (inputString) !== 'string';
+    }
 
-    return total;
+    isNotWellFormed(numbers, separator) {
+        let regexp = new RegExp(`^(-)?\\d+((${separator})(-)?\\d+)*$`);
+        return !regexp.test(numbers);
+    }
 
-  }
+    splitSeparator(inputString) {
+        if (!/^\/\/.*\n/.test(inputString))
+            return { numbersString: inputString, separator: ',|\n' };
 
-  invalidArgumentException() {
-    throw Error('Invalid argument')
-  }
+        let [head, ...tail] = inputString.split('\n');
+        let separator = this.getSeparator(head.substring(2, head.length));
+        return { numbersString: tail.join(), separator };
+    }
 
+    getSeparator(separatorString) {
+        return separatorString
+            .replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+            .replace(/\\\]\\\[/g, '|')
+            .replace(/^\\\[/, '')
+            .replace(/\\\]$/, '');
+    }
+
+    extractNumbers(numbersString, separator) {
+        return numbersString.split(new RegExp(separator))
+            .map((item) => parseInt(item));
+    }
+
+    containsNegativeNumber(numbers) {
+        return numbers.some((item) => item < 0);
+    }
+
+    getTotal(numbers, separator) {
+        return numbers
+            .filter((item) => item < 1000)
+            .reduce((memo, item) => memo + item);
+    }
+
+    invalidArgumentError() {
+        throw Error('Invalid argument');
+    }
+
+    negativesNotAllowedError() {
+        throw Error('Negatives not allowed');
+    }
 }
 
 module.exports = Calculator;
--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -3,74 +3,65 @@ const Calculator = require('../source/calculator');
 
 describe('Calculator', () => {
 
-  let calculator;
+    let calculator;
 
-  beforeEach(() => {
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
 
-    calculator = new Calculator();
+    it('should return zero with empty string', () => {
+        checkIO('', 0);
+    });
 
-  });
+    it('should sum with just one number', () => {
+        checkIO('1', 1);
+    });
 
-  it('should return zero with empty string', () => {
+    it('should sum with two numbers separated by comma', () => {
+        checkIO('1,2', 3);
+    });
 
-    checkIO('', 0);
+    it('should throw exception if pass undefined', () => {
+        checkError(calculator.add.bind(calculator), 'Invalid argument');
+    });
 
-  });
+    it('should throw exception if pass not well formed string', () => {
+        checkError(calculator.add.bind(calculator, 'a1b2c3'), 'Invalid argument');
+        checkError(calculator.add.bind(calculator, '1,\n'), 'Invalid argument');
+    });
 
-  it('should sum with just one number', () => {
+    it('should sum with n numbers', () => {
+        checkIO('1,2,10,20', 33);
+    });
 
-    checkIO('1', 1);
+    it('should sum with n numbers separated by comma and \\n', () => {
+        checkIO('1\n2,3', 6);
+    });
 
-  });
+    it('should define delimiter', () => {
+        checkIO('//;\n1;2', 3);
+    });
 
-  it('should sum with two numbers separated by comma', () => {
+    it('should throw exception if pass negative values', () => {
+        checkError(calculator.add.bind(calculator, '1,-4'), "Negatives not allowed");
+    });
 
-    checkIO('1,2', 3);
+    it('should ignore value grather then 1000', () => {
+        checkIO('1000,2', 2);
+    });
 
-  });
+    it('should support advanced delimiters', () => {
+        checkIO('//***\n1***2', 3);
+        checkIO('//[*][%]\n1*2%3', 6);
+    });
 
-  it('should throw exception if pass undefined', () => {
+    function checkIO(input, output) {
+        let actual = calculator.add(input);
+        expect(actual).to.equal(output);
+    }
 
-    checkError(calculator.add.bind(calculator));
-
-  });
-
-  it('should throw exception if pass not well formed string', () => {
-
-    checkError(calculator.add.bind(calculator, 'a1b2c3'));
-    checkError(calculator.add.bind(calculator, '1,\n'));
-
-  });
-
-  it('should sum with n numbers', () => {
-
-    checkIO('1,2,10,20', 33);
-
-  });
-
-  it('should sum with n numbers separated by comma and \\n', () => {
-
-    checkIO('1\n2,3', 6);
-
-  });
-
-  it('should define delimiter', () => {
-
-    checkIO('//;\n1;2', 3);
-    
-  });
-
-  function checkIO(input, output) {
-
-    let actual = calculator.add(input);
-    expect(actual).to.equal(output);
-
-  }
-
-  function checkError(input) {
-
-    expect(input).to.throw(Error, 'Invalid argument');
-
-  }
+    function checkError(input, errorMessage) {
+        expect(input).to.throw(Error, errorMessage);
+    }
 
 });
