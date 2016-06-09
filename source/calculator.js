5db69a0b15e6a52c7cfd2f1d8fbdd6cb3e751ec6
Carmine <cdente@codearchitects.com>
2016-06-09 16:00:50 +0200

    Fabio avrà capito?

--- source/calculator.js
+++ source/calculator.js
@@ -2,16 +2,18 @@ class Calculator {
 
   add(numbers) {
 
-    if (numbers.length === 0) {
-      return 0;
-    } else {
-
-      return numbers.split(',')
-        .map((item)=> parseInt(item))
-        .reduce((memo, item) => memo + item);
+    if (typeof (numbers) !== 'string') {
+      throw Error('Invalid argument');
     }
 
-  };
+    if (numbers.length === 0) {
+      return 0;
+    }
+
+    return numbers.split(',')
+      .map((item) => parseInt(item))
+      .reduce((memo, item) => memo + item);
+  }
 
 }
 
--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -13,26 +13,44 @@ describe('Calculator', () => {
 
   it('should return zero with empty string', () => {
 
-    let actual = calculator.add('');
-
-    expect(actual).to.equal(0);
+    checkIO('', 0);
 
   });
 
   it('should sum with just one number', () => {
 
-    let actual = calculator.add('1');
-
-    expect(actual).to.equal(1);
+    checkIO('1', 1);
 
   });
 
   it('should sum with two numbers', () => {
 
-    let actual = calculator.add('1,2');
-
-    expect(actual).to.equal(3);
+    checkIO('1,2', 3);
 
   });
+
+  it('should throw exception if pass undefined', () => {
+
+    expect(calculator.add).to.throw('Invalid argument');
+
+  });
+
+  // ^(\d(,\d)?)?$
+  // xit('should throw exception if pass not well formed string', () => {
+  //   expect(calculator.add.bind(calculator, 'a1b2c3')).to.throw('Invalid argument');
+  // });
+
+  it('should sum with n numbers', () => {
+
+    checkIO('1,2,10,20', 33);
+
+  });
+
+  function checkIO(input, output) {
+
+    let actual = calculator.add(input);
+    expect(actual).to.equal(output);
+
+  }
 
 });
